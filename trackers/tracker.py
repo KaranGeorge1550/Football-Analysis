c00331a7130684850c355a7cb50d29226e86deba
Karan George <39949919+KaranGeorge1550@users.noreply.github.com>
2024-06-02 17:18:43 +1200

    Track classes from frames

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -13,11 +13,17 @@ class Tracker:
             detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
             detections += detections_batch
     
-    def get_object_tracks(self, frames):
-        detections = self.detect_frames(frames)
+    def get_object_tracks(self, frame_num):
+        detections = self.detect_frames(frame_num)
+
+        tracks={
+            "players": [],
+            "ball": [],
+            "referees": [],
+        }
 
          #Overwrite goalkeeper class with player class
-        for frame, detection in enumerate(detections):
+        for frame_num, detection in enumerate(detections):
             class_names = detections.names
             class_names_inverse = {v: k for k, v in class_names.items()}
 
@@ -28,3 +34,24 @@ class Tracker:
             for object, class_id in detection_supervision.classes.items():
                 if class_names[class_id] == 'goalkeeper':
                     detection_supervision.class_id[object] = class_names_inverse['player']
+
+            # Track
+            detection_tracking = self.tracker.update_with_detections(detection_supervision)
+
+            tracks["players"].append({})
+            tracks["ball"].append({})
+            tracks["referees"].append({})
+
+            for frame_num in detection_tracking:
+                bounding_box = frame_num[0].tolist()
+                class_id = frame_num[3]
+                tracker_id = frame_num[4]
+
+                if class_id == class_names_inverse['player']:
+                    tracks["players"][frame_num][tracker_id] = {"bounding_box":bounding_box}
+                elif class_id == class_names_inverse['ball']:
+                    tracks["ball"][frame_num][1] = {"bounding_box":bounding_box}
+                elif class_id == class_names_inverse['referee']:
+                    tracks["referees"][frame_num][tracker_id] = {"bounding_box":bounding_box} 
+
+        return tracks 
