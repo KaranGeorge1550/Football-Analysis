694debe46309a8310bc05e28afa9a9fcb79a0f37
Karan George <39949919+KaranGeorge1550@users.noreply.github.com>
2024-06-02 17:31:38 +1200

    Initalise tracker

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from utils import read_video, save_video
+from trackers import Tracker
+
 
 def main():
     video_frames = read_video('input_videos/hockey_input1.mp4')
+
+    # Initialise tracker
+    tracker = Tracker('models/best.pt')
+    tracks = tracker.get_object_tracks(video_frames, 
+                                       read_from_path=True, 
+                                       path='tracks/tracks.pkl')
 
     save_video(video_frames, 'output_videos/output_vid.avi')
 
